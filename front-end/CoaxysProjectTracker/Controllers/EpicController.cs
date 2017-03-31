9079a52546d32fdb19dfde75405eac56808c7a49
bidjonino <bidjonino@gmail.com>
2017-03-31 16:15:20 +0200

    Front-End : Add Project Model (Epic / Task / status / story )

---
+++ front-end/CoaxysProjectTracker/Controllers/EpicController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoaxysProjectTracker.Controllers
+{
+    public class EpicController : Controller
+    {
+        // GET: Epic
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
