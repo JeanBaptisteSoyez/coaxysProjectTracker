48fef84fca1ba3b00f97789b613ef78763fd9264
Camille Payen <camille.payen.pro@gmail.com>
2017-04-02 18:07:31 +0200

    Frontend - Add a repository for projects

---
+++ front-end/CoaxysProjectTracker/Repositories/ProjectRepository.cs
@@ -0,0 +1,45 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoaxysProjectTracker.Api;
+
+namespace CoaxysProjectTracker.Entities
+{
+    class ProjectRepository
+    {
+        private List<Project> projects;
+
+        protected async Task<List<Project>> LoadProjects()
+        {
+            projects = await API.GetAsync<List<Project>>("project.json");
+            return projects;
+        }
+
+        public IEnumerable<Project> GetProjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Project GetProjectByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertProject()
+        {
+
+        }
+
+        public void UpdateProject()
+        {
+
+        }
+
+        public void DeleteProject()
+        {
+
+        }
+    }
+}*/
